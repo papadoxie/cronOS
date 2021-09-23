9a0a4ea3c156e1946e5f547209992fc21e9a9de8
papadoxie <nofilqasim@gmail.com>
2021-09-23 17:58:52 +0500

    Removed puts prototype from kernel.c

--- kernel/arch/i386/source/kernel.c
+++ kernel/arch/i386/source/kernel.c
@@ -1,7 +1,5 @@
 #include "../include/types.h"
-#include "../include/kstdlib.h"
-
-void puts(const char *str);
+#include "../include/kstdio.h"
 
 void kmain(void *multiboot_structure, uint32_t magicnumber)
 {
