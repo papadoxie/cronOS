8705aaa52905527b155d49fb92cc178b804896bd
papadoxie <nofilqasim@gmail.com>
2021-09-23 15:18:18 +0500

    Added GDT structs and gdt_init

---
+++ kernel/arch/i386/include/gdt.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifndef __GDT_H__
+#define __GDT_H__
+
+#define GDT_BASE 0x00000800
+
+struct __segment_descriptor
+{
+    uint16_t limit_lo;       // Lower 2 bytes of Segment Limit
+    uint16_t base_lo;        // Lower 2 bytes of Base Address
+    uint8_t base_hi;         // Upper byte of Base Address
+    uint8_t flags;           // Flags (Access Rights)
+    uint8_t limit_hi : 4;    // Upper 4 bits of Segment Limit
+    uint8_t flags_extra : 4; // Upper 4 bits for Flags
+    uint8_t base_vhi;        // Highest byte of Base Address
+} __attribute__((packed));
+
+typedef struct __global_descriptor_table
+{
+    uint32_t base_addr; // Complete Base Address
+    uint16_t seg_limit; // Complete Segment Limit
+
+    struct __segment_descriptor null; // Null Segment
+
+    struct __segment_descriptor k_code;  // Kernel Code Segment
+    struct __segment_descriptor k_data;  // Kernel Data Segment
+    struct __segment_descriptor k_stack; // Kernel Stack Segment
+
+    struct __segment_descriptor u_code;  // User Code Segment
+    struct __segment_descriptor u_data;  // User Data Segment
+    struct __segment_descriptor u_stack; // User Stack Segment
+
+    struct __segment_descriptor tss; // Task State Segment
+
+} GlobalDescriptorTable;
+
+void gdt_init(GlobalDescriptorTable *gdt);
+
+#endif
---
+++ kernel/arch/i386/source/gdt.c
@@ -0,0 +1,16 @@
+#include "../include/gdt.h"
+
+void init_segment_desc(uint32_t base_addr,
+                       uint32_t seg_limit,
+                       uint8_t flags,
+                       uint8_t flags_extra,
+                       struct __segment_descriptor *seg_desc)
+{
+    seg_desc->limit_lo = (seg_limit & 0xffff);
+    seg_desc->base_lo = (base_addr & 0xffff);
+    seg_desc->base_hi = (base_addr & 0xff0000) >> 16;
+    seg_desc->flags = flags;
+    seg_desc->limit_hi = (seg_limit & 0xf0000) >> 16;
+    seg_desc->flags_extra = (flags_extra & 0xf);
+    seg_desc->base_vhi = (base_addr & 0xff000000) >> 24;
+}
