16813536a1676fb2d25c16edb01f3ae734981304
papadoxie <nofilqasim@gmail.com>
2021-09-23 15:45:02 +0500

    Renamed puts to printk

--- kernel/arch/i386/source/kernel.c
+++ kernel/arch/i386/source/kernel.c
@@ -4,12 +4,12 @@ void puts(const char *str);
 
 void kmain(void *multiboot_structure, uint32_t magicnumber)
 {
-    puts("--------------------This is a test of the operating system--------------------");
+    printk("--------------------This is a test of the operating system--------------------");
     while (1)
         ;
 }
 
-void puts(const char *str)
+void printk(const char *str)
 {
     uint16_t *vidmem = (uint16_t *)0xb8000;
 
