dbafac63fa973d6effdc8811a56792b7770279aa
papadoxie <nofilqasim@gmail.com>
2021-09-26 00:09:58 +0500

    Made kmain a noreturn method

--- kernel/arch/i386/source/kernel.c
+++ kernel/arch/i386/source/kernel.c
@@ -4,5 +4,6 @@
 __attribute__((noreturn)) void kmain(void *multiboot_structure, uint32_t magicnumber)
 {
     printk("--------------------This is a test of the operating system--------------------");
-    
+    while(1)
+        ;
 }
