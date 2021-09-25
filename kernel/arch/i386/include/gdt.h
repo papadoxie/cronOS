bca2fecfd5817d272f1b0c9f43dfb2e94993ed8b
papadoxie <nofilqasim@gmail.com>
2021-09-26 03:08:54 +0500

    Improved Comments

--- kernel/arch/i386/include/gdt.h
+++ kernel/arch/i386/include/gdt.h
@@ -5,12 +5,13 @@
 #ifndef __GDT_H__
 #define __GDT_H__
 
+//* Segment Start and End Limits
 #define BASE_ADDR 0x0           // Base Address of Segment
 #define SEG_LIMIT_CS 0xf0000000 // Segment Limit of Code Segment
 #define SEG_LIMIT_DS 0xf0000000 // Segment Limit of Data Segment
 #define SEG_LIMIT_SS 0x0        // Segment Limit of Stack Segment
 
-// Access Bits
+//* Access Bits
 #define ACS_PRESENT 0x80   // Present Bit
 #define ACS_R0 0x00        // Privilege Level 0
 #define ACS_R1 0x20        // Privilege Level 1
@@ -22,7 +23,7 @@
 #define ACS_RW 0x02        // Read/Write Bit
 #define ACS_ACCESS 0x01    // Accessed Bit
 
-// Flags
+//* Flags
 #define FLG_GB 0x08   // 4 KiB limit multiplier
 #define FLG_SB 0x04   // 32 bit protected mode
 #define FLG_LONG 0x02 // Long Mode
@@ -44,29 +45,30 @@ typedef struct __global_descriptor_table
     uint32_t base_addr; // Complete Base Address
     uint16_t seg_limit; // Complete Segment Size
 
-    // Null Segment
+    //* Null Segment
     struct __segment_descriptor null;
 
-    // Kernelmode Segments
+    //* Kernelmode Segments
     struct __segment_descriptor k_cs; // Kernel Code Segment
     struct __segment_descriptor k_ds; // Kernel Data Segment
     struct __segment_descriptor k_ss; // Kernel Stack Segment
 
-    // Usermode Segments
+    //* Usermode Segments
     struct __segment_descriptor u_cs; // Code Segment
     struct __segment_descriptor u_ds; // Data Segment
     struct __segment_descriptor u_ss; // Stack Segment
 
-    // Task State Segments
+    //* Task State Segments
     struct __segment_descriptor m_tss;  // Multitask Handling
     struct __segment_descriptor df_tss; // Double Fault Handling
 
 } GlobalDescriptorTable;
 
+//* GDT Functions
 void gdt_init(GlobalDescriptorTable *gdt);
 void gdt_destroy(GlobalDescriptorTable *gdt);
 
-uint16_t cs_offset(GlobalDescriptorTable *gdt);
-uint16_t ds_offset(GlobalDescriptorTable *gdt);
+uint16_t get_cs_offset(GlobalDescriptorTable *gdt); 
+uint16_t get_ds_offset(GlobalDescriptorTable *gdt);
 
 #endif //__GDT_H__
