25d6f69c6f13317f6ffa5d62ac503720ea998c65
papadoxie <nofilqasim@gmail.com>
2021-09-26 20:43:46 +0500

    Added gdt_init to kmain

--- kernel/arch/i386/source/kernel.c
+++ kernel/arch/i386/source/kernel.c
@@ -1,9 +1,15 @@
 #include "../include/types.h"
 #include "../include/kstdio.h"
+#include "../include/gdt.h"
+
+struct __global_descriptor_table gdt;
 
 __attribute__((noreturn)) void kmain(void *multiboot_structure, uint32_t magicnumber)
 {
     printk("--------------------This is a test of the operating system--------------------");
-    while(1)
+    printk("Loading GDT...                                                                ");
+    __gdt_init(&gdt);
+    printk("GDT Loaded Successfully...                                                    ");
+    while (1)
         ;
 }
