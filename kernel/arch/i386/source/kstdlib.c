cc16ccf27bde0e105edf40da5629eead14d9ad1d
papadoxie <nofilqasim@gmail.com>
2021-09-27 23:05:25 +0500

    Removed __restrict__ attribute

--- kernel/arch/i386/source/kstdlib.c
+++ kernel/arch/i386/source/kstdlib.c
@@ -1,6 +1,6 @@
 #include "../include/kstdlib.h"
 
-void *kmemset(char *__restrict__ __dest, char __src, size_t __size)
+void *kmemset(char *__dest, char __src, size_t __size)
 {
     char *ret_block = __dest;
 
@@ -8,18 +8,16 @@ void *kmemset(char *__restrict__ __dest, char __src, size_t __size)
     {
         *__dest++ = __src;
     }
-    __dest = NULL;
     return ret_block;
 }
 
-void *kmemcpy(char *__restrict__ __dest, char *__restrict__ __src, size_t __size)
+void *kmemcpy(char *__dest, char *__src, size_t __size)
 {
     char *ret_block = __dest;
 
     while (__size--)
     {
-        *__dest++ = *__src;
+        *__dest++ = *__src++;
     }
-    __dest = NULL;
     return ret_block;
 }
