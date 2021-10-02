dd7bacddf4a78a2b870f43dc412594138f9e4d9c
papadoxie <nofilqasim@gmail.com>
2021-10-02 07:23:41 +0500

    Added scrolling to printk

--- kernel/arch/i386/include/kstdio.h
+++ kernel/arch/i386/include/kstdio.h
@@ -5,6 +5,11 @@
 #ifndef __K_IO_H__
 #define __K_IO_H__
 
+#define VIDEO_MEMORY 0xB8000
+
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25
+
 void printk(const char *__str);
 
 #endif //__K_IO_H__
--- kernel/arch/i386/source/kstdio.c
+++ kernel/arch/i386/source/kstdio.c
@@ -1,11 +1,46 @@
 #include "../include/kstdio.h"
 
+void shift_screen_buffer_up(char *screen_buffer)
+{
+    for (uint16_t i = 0; i < SCREEN_HEIGHT; i++)
+    {
+        for (uint16_t j = 0; j < SCREEN_WIDTH; j++)
+        {
+            screen_buffer[(SCREEN_WIDTH * i) + j] = screen_buffer[(SCREEN_WIDTH * (i + 1)) + j];
+        }
+    }
+}
+
 void printk(const char *__str)
 {
-    uint16_t *vidmem = (uint16_t *)0xb8000;
+    uint16_t *vidmem = (uint16_t *)VIDEO_MEMORY;
+    static char screen_buffer[(SCREEN_WIDTH * SCREEN_HEIGHT) + SCREEN_WIDTH];
+    static uint8_t x = 0;
+    static uint8_t y = 0;
 
     for (uint16_t i = 0; __str[i] != '\0'; i++)
     {
-        vidmem[i] = (vidmem[i] & 0xFF00) | __str[i];
+        if (__str[i] == '\n' || x == SCREEN_WIDTH)
+        {
+            x = 0;
+            y++;
+        }
+
+        if (y == SCREEN_HEIGHT)
+        {
+            y--;
+            shift_screen_buffer_up(screen_buffer);
+        }
+
+        screen_buffer[(SCREEN_WIDTH * y) + x] = __str[i];
+        x++;
+    }
+
+    x = 0;
+    y++;
+
+    for (uint16_t i = 0; i < ((SCREEN_WIDTH * SCREEN_HEIGHT) + SCREEN_WIDTH); i++)
+    {
+        vidmem[i] = (vidmem[i] & 0xFF00) | screen_buffer[i];
     }
 }
