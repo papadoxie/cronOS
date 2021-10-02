b4671626e1530ffbf2c42afeaab339b88614debc
papadoxie <nofilqasim@gmail.com>
2021-10-02 07:30:16 +0500

    Added comments

--- kernel/arch/i386/include/kstdio.h
+++ kernel/arch/i386/include/kstdio.h
@@ -5,11 +5,14 @@
 #ifndef __K_IO_H__
 #define __K_IO_H__
 
+// Start of the video memory
 #define VIDEO_MEMORY 0xB8000
 
+// Screen dimensions
 #define SCREEN_WIDTH 80
 #define SCREEN_HEIGHT 25
 
+// Basic kernel print
 void printk(const char *__str);
 
 #endif //__K_IO_H__
--- kernel/arch/i386/source/kstdio.c
+++ kernel/arch/i386/source/kstdio.c
@@ -1,5 +1,6 @@
 #include "../include/kstdio.h"
 
+// Shift the screen buffer up by one line
 void shift_screen_buffer_up(char *screen_buffer)
 {
     for (uint16_t i = 0; i < SCREEN_HEIGHT; i++)
@@ -13,8 +14,11 @@ void shift_screen_buffer_up(char *screen_buffer)
 
 void printk(const char *__str)
 {
+    // Cast buffer address to pointer for easier use
     uint16_t *vidmem = (uint16_t *)VIDEO_MEMORY;
     static char screen_buffer[(SCREEN_WIDTH * SCREEN_HEIGHT) + SCREEN_WIDTH];
+
+    // Cursor position
     static uint8_t x = 0;
     static uint8_t y = 0;
 
@@ -36,9 +40,11 @@ void printk(const char *__str)
         x++;
     }
 
+    // Move cursor to new line after printing
     x = 0;
     y++;
 
+    // Copy screen buffer to video memory
     for (uint16_t i = 0; i < ((SCREEN_WIDTH * SCREEN_HEIGHT) + SCREEN_WIDTH); i++)
     {
         vidmem[i] = (vidmem[i] & 0xFF00) | screen_buffer[i];
