ea15d5e144f8850999f255498cb02dbd70c3ebc4
papadoxie <nofilqasim@gmail.com>
2021-10-21 02:31:17 +0500

    Tested \n

--- kernel/arch/i386/source/kernel.c
+++ kernel/arch/i386/source/kernel.c
@@ -1,13 +1,15 @@
 #include <types.h>
+#include <tty.h>
 #include <kstdio.h>
 #include <gdt.h>
 
 __attribute__((noreturn)) void kmain(void *multiboot_structure, uint32_t magicnumber)
 {
-    kprintf("--------------------This is a test of the operating system--------------------");
-    kprintf("Loading GDT...");
+    tty_init();
+    kprintf("--------------------This is a test of the operating system--------------------\n");
+    kprintf("Loading GDT...\n");
     __gdt_init();
-    kprintf("GDT Loaded Successfully...");
+    kprintf("GDT Loaded Successfully...\n");
     while (1)
         ;
 }
