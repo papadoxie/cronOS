07cd713ac4cf719896e4afa91f344a97d3a5a59b
papadoxie <nofilqasim@gmail.com>
2021-10-21 02:15:15 +0500

    Added definitions for EOI and EOVERFLOW

--- kernel/libk/include/kstdio.h
+++ kernel/libk/include/kstdio.h
@@ -6,10 +6,12 @@
 #define __K_IO_H__
 
 #define EOF -1
+#define EOVERFLOW -1
+#define EIO -1
 
 // Basic kernel prints
 void printk(const char *__str);
-int kprintf(const char* __restrict__ __format, ...);
+int kprintf(const char *__restrict__ __fmt_str, ...);
 int putchar(int __char);
 
 #endif //__K_IO_H__
