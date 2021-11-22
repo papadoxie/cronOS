946e37f8d8ba3d54f85d44113dd3667ee608cbf3
papadoxie <nofilqasim@gmail.com>
2021-11-22 23:03:24 +0500

    Added cdefs.h

--- kernel/libk/include/kstdlib.h
+++ kernel/libk/include/kstdlib.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <types.h>
-
 #ifndef __K_STD_LIB_H__
 #define __K_STD_LIB_H__
+
+#include <types.h>
+#include <sys/cdefs.h>
 
 void *kmemset(char *__restrict__ __dest, char __src, size_t __size);
 void *kmemcpy(char *__restrict__ __dest, char *__restrict__ __src, size_t __size);
