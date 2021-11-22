414c5b8d882e8537286c7f0616cc956ad1a0bfce
papadoxie <nofilqasim@gmail.com>
2021-11-23 00:30:55 +0500

    Tested kmain with __builtin_unreachable()

--- kernel/arch/i386/source/kernel.c
+++ kernel/arch/i386/source/kernel.c
@@ -8,14 +8,13 @@ __attribute__((noreturn)) void kmain(void *multiboot_structure, uint32_t magicnu
     //Initialize the tty
     __tty_init();
     printk("--------------------This is a test of the operating system--------------------");
-    
+
     //Initialize the GDT
     printk("Loading GDT...");
     __gdt_init();
     printk("GDT Loaded Successfully...");
-    
 
-    // Never return
-    end:
+// Never return
+end:
     goto end;
 }
