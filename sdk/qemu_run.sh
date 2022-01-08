9fcd346d1320d407fb59392f3d108b28b7ccf28c
papadoxie <nofilqasim@gmail.com>
2022-01-08 11:55:24 +0500

    Added a debug flag

--- sdk/qemu_run.sh
+++ sdk/qemu_run.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
-qemu-system-$AARCH \
--boot d \
--cdrom $KISO \
--m 512 \
--s -S \
-&
+if [ "$DDEBUG" == "1" ] 
+then
+    qemu-system-$AARCH \
+    -boot d \
+    -cdrom $KISO \
+    -m 512 \
+    -s -S \
+    &
+
+    gdb kernel/bin/kernel.bin \
+    -ex "target remote :1234" \
+    -ex "break _start" \
+    -ex "continue" \
+
+else 
+    qemu-system-$AARCH \
+    -boot d \
+    -cdrom $KISO \
+    -m 512 \
+    &
+fi
+
