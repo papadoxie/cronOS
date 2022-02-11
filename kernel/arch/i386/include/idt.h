1f139a484ad441b5f43db59fb7e759a37cac0ee2
papadoxie <nofilqasim@gmail.com>
2022-02-12 04:16:08 +0500

    added idt struct

--- kernel/arch/i386/include/idt.h
+++ kernel/arch/i386/include/idt.h
@@ -3,4 +3,14 @@
 #ifndef __IDT_H__
 #define __IDT_H__
 
+struct __idt
+{
+    unsigned short base_lo;
+    unsigned short sel;
+    unsigned char always0;
+    unsigned char flags;
+    unsigned short base_hi;
+} __attribute__((packed));
+
+
 #endif // __IDT_H__
